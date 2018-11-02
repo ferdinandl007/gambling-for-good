4eb9a1a497653bd9bb19ba8f67ed7c02b69aead4
Jonathan Wood <jonn.paul.w@gmail.com>
2018-11-01 21:35:02 -0400

    contract compiles, is createable. Constructor works as expeted. playGame() runs not as expected, payout does not seem to run. No funds are payed out

--- contracts/game.sol
+++ contracts/game.sol
@@ -1,6 +1,6 @@
 pragma solidity^0.4.22;
-contract wallet{
-    uint playerBid = playerBid * 100;
+contract game{
+    uint playerBid;
     uint playerPayout = 0;
     uint devPayout = 0;
     uint charityPayout = 0;
@@ -10,12 +10,25 @@ contract wallet{
     address charityAddress;
     uint winMultiplier = 120;//includes two decimals out
     
+    constructor(address charity, uint seed)
+    public
+    payable
+    {
+        devAddress = 0xF5e494ea47FA500D5d4ABf2d6270A4F18aa44875;//TODO: hardcode address
+        charityAddress = charity;
+        playerAddress = msg.sender;
+        playerBid = msg.value * 100;
+        playerSeed = seed;
+        playGame();
+    }
+    
     //playGame
     //Consumes: playerBid and playerSeed, to decide winner
     //Produces: nothing
     //side effects: modifies values of all payouts
-    function playGame(uint playerBid, uint playerSeed)
+    function playGame()
     public
+    payable
     returns (bool) {
         bool winState = false;
         if(playerSeed % 100 < 45){
@@ -32,6 +45,7 @@ contract wallet{
             devPayout = (playerBid * devWithholding)/100;
             playerPayout = 0;
         }
+        payout();
         return winState;
     }
     
@@ -39,7 +53,8 @@ contract wallet{
     //Consumes: the payouts and addresses of each userSeed
     //Produces: true if success, false otherwise
     //side effects: pays balances out to each user, transfering funds
-    function payout(uint playerPayout, uint devPayout, uint charityPayout, address playerAddress, address devAddress, address charityAddress)
+    function payout()
+    payable
     public
     returns (bool) {
         if(playerAddress.send(playerPayout) && charityAddress.send(charityPayout) && devAddress.send(devPayout)){
